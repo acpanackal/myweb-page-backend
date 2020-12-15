bbc1675c9de01e96b709543d2e40fc7cbead94ca
Akhila Panackal <akhilacyriacpanackal@gmail.com>
2020-12-15 16:10:19 +0100

    With client build

---
+++ src/build/precache-manifest.d1fad8a8616bf17628f124c6d3b44cad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f3880793fbf6711063",
+    "url": "/static/css/main.0a0a7c5f.chunk.css"
+  },
+  {
+    "revision": "63f3880793fbf6711063",
+    "url": "/static/js/main.7965fe54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76173f92fa92223b2020",
+    "url": "/static/js/2.e0b0ed77.chunk.js"
+  },
+  {
+    "revision": "f0169062ef0105eb6f058ab40fb3c4ab",
+    "url": "/index.html"
+  }
+];
